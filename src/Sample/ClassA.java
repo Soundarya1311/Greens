63f640ae802b754a4aaa0bb4635d4a33208379c0
Soundarya1311 <soundaryakumar76@gmail.com>
2022-03-01 14:52:46 +0530

    dobmethod

--- src/Sample/ClassA.java
+++ src/Sample/ClassA.java
@@ -5,8 +5,13 @@ public class ClassA {
 	private void usrname() {
 System.out.println("Soundarya");	
 	}
+	
+	private void dateOfBirth() {
+		System.out.println("dob");
+	}
 public static void main(String[] args) {
 	ClassA a = new ClassA();
 	a.usrname();
+	a.dateOfBirth();
 }
 }
