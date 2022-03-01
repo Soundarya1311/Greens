5e56daf2bec080b4a6feaf0e01048f8708d36f78
Soundarya1311 <soundaryakumar76@gmail.com>
2022-03-01 14:00:41 +0530

    folder structure

---
+++ src/Sample/ClassA.java
@@ -0,0 +1,12 @@
+package Sample;
+
+public class ClassA {
+	
+	private void usrname() {
+System.out.println("Soundarya");	
+	}
+public static void main(String[] args) {
+	ClassA a = new ClassA();
+	a.usrname();
+}
+}
